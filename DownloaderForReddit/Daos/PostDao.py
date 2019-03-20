742d457ffce5cf688bfe9cff3e9915e8b22dbbb5
Kyle <kyle.hickey222@gmail.com>
2019-03-20 07:29:33 -0500

    Update post dao to include more useful queries

--- DownloaderForReddit/Daos/PostDao.py
+++ DownloaderForReddit/Daos/PostDao.py
@@ -15,9 +15,27 @@ class PostDao(BaseDao):
 
     def get_all_posts(self):
         posts = []
-        sql = "SELECT * FROM posts"  # TODO: this will need to have a limit and order
         c = self.get_cursor()
+        sql = "SELECT * FROM posts"  # TODO: this will need to have a limit and order
         c.execute(sql)
+        row = c.fetchone()
+        while row is not None:
+            posts.append(self.build_post(row))
+            row = c.fetchone()
+        return posts
+
+    def get_posts_for_user(self, user):
+        sql = "SELECT * FROM posts WHERE id IN (SELECT post_id from user_posts WHERE user_id=?)"
+        return self.get_posts(sql, user.id)
+
+    def get_posts_for_subreddit(self, subreddit):
+        sql = "SELECT * FROM posts WHERE id IN (SELECT post_id FROM subreddit_posts WHERE posts.subreddit_id=?)"
+        return self.get_posts(sql, subreddit.id)
+
+    def get_posts(self, sql, id_value):
+        posts = []
+        c = self.get_cursor()
+        c.execute(sql, (id_value, ))
         row = c.fetchone()
         while row is not None:
             posts.append(self.build_post(row))
@@ -42,26 +60,28 @@ class PostDao(BaseDao):
 
     def build_post(self, row):
         post = Post(
-            row['id'],
-            row['title'],
-            row['author_id'],
-            row['author_name'],
-            row['subreddit_id'],
-            row['subreddit_name'],
-            row['creation_date'],
+            title=row['title'],
+            _id=row['id'],
+            author_id=row['author_id'],
+            author=row['author_name'],
+            subreddit_id=row['subreddit_id'],
+            subreddit=row['subreddit_name'],
+            created=row['creation_date'],
+            domain=row['domain'],
+            score=row['score'],
+            text=row['text'],
+            self_post=row['self_post'],
+            date_added=row['date_added']
         )
-        post.text = row['text']
-        post.score = row['score']
-        post.date_added = row['date_added']
         return post
 
     def add_post(self, post):
         c = self.get_cursor()
         try:
-            sql = "INSERT INTO posts (title, author_id, author_name, subreddit_id, subreddit_name, score, " \
-                  "creation_date, text) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-            c.execute(sql, (post.title, post.author_id, post.author, post.subreddit_id, post.subreddit, post.score,
-                            post.created))
+            sql = "INSERT INTO posts (title, author_id, author_name, subreddit_id, subreddit_name, domain, score, " \
+                  "creation_date, text, self_post, date_added) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            c.execute(sql, (post.title, post.author_id, post.author, post.subreddit_id, post.subreddit, post.domain,
+                            post.score, post.created, post.text, post.self_post, time.time()))
             return c.rowcount > 0
         except IntegrityError:
             self.logger.error("Failed to add post to database", extra={'post_title': post.title})
@@ -70,10 +90,10 @@ class PostDao(BaseDao):
     def update_post(self, post):
         c = self.get_cursor()
         try:
-            sql = "UPDATE posts SET title=?, author_id=?, author_name=?, subreddit_id=?, subreddit_name=?, score=?, " \
-                  "creation_date=?, text=? WHERE id=?"
-            c.execute(sql, post.title, post.author_id, post.author, post.subreddit_id, post.subreddit, post.score,
-                      post.created, post.text, post.id)
+            sql = "UPDATE posts SET title=?, author_id=?, author_name=?, subreddit_id=?, subreddit_name=?, domain=?, " \
+                  "score=?, creation_date=?, text=?, self_post=? WHERE id=?"
+            c.execute(sql, post.title, post.author_id, post.author, post.subreddit_id, post.domain, post.subreddit,
+                      post.score, post.created, post.text, post.self_post, post.id)
             return c.rowcount > 0
         except IntegrityError:
             self.logger.error("Failed to update post", extra={'post_title': post.title})
