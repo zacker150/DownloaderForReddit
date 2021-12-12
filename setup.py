d511e4ec9df0015985654251c5082a28f9b1851d
zacker150 <zacker150@hotmail.com>
2021-12-11 20:01:57 -0600

    Setuptools install

--- setup.py
+++ setup.py
@@ -2,13 +2,43 @@ from setuptools import setup
 from DownloaderForReddit.version import __version__
 
 setup(
+    # Metadata
     name='DownloaderForReddit',
     version=__version__,
-    packages=[''],
     url='https://github.com/MalloyDelacroix/DownloaderForReddit',
     license='GNU GPLv3',
     author='Kyle Hickey',
     author_email='kyle.hickey222@gmail.com',
     description='A GUI application with some advanced features that downloads user posted content from reddit, '
-                'either via a list of users or subreddits.'
+                'either via a list of users or subreddits.',
+
+    packages=[
+        'DownloaderForReddit',
+        'DownloaderForReddit.core',
+        'DownloaderForReddit.customwidgets',
+        'DownloaderForReddit.database',
+        'DownloaderForReddit.extractors',
+        'DownloaderForReddit.gui',
+        'DownloaderForReddit.gui.database_views',
+        'DownloaderForReddit.gui.settings',
+        'DownloaderForReddit.gui.widgets',
+        'DownloaderForReddit.guiresources',
+        'DownloaderForReddit.guiresources.database_views',
+        'DownloaderForReddit.guiresources.settings',
+        'DownloaderForReddit.guiresources.widgets',
+        'DownloaderForReddit.local_logging',
+        'DownloaderForReddit.messaging',
+        'DownloaderForReddit.persistence',
+        'DownloaderForReddit.scheduling',
+        'DownloaderForReddit.utils',
+        'DownloaderForReddit.utils.exporters',
+        'DownloaderForReddit.utils.importers',
+        'DownloaderForReddit.viewmodels',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'dfr = main:main',
+        ],
+    }
 )
